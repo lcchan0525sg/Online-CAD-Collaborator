0f56649e95d7cf1e5f1f13cc72b6bfaf87d535da
chan_ <chan_@local>
2026-08-17 21:02:53 +0800

    Add start.bat launcher for the project folder

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+set "PORT=4322"
+if not "%~1"=="" set "PORT=%~1"
+if exist "port.txt" set /p PORT=<port.txt
+
+echo Starting CAD Viewer on port %PORT% ...
+start "" http://localhost:%PORT%/
+set PORT=%PORT%
+node server.js
+pause
